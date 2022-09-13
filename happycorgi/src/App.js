272fb3e86dd840aa46f5c13506bf66c1e0494ed6
Jenai Charlton <jenaicharlton@gmail.com>
2022-09-13 19:51:51 -0400

    code commit

---
+++ happycorgi/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Header from './components/Header';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    <div class="flex-grid">
+    <div class="column">
+    <img src="//placehold.it//600x300" alt=""/>
+        
+        
+    </div>
+       
+    </div>
+        </div>
+    
+  );
+}
+
+export default App;
---
+++ happycorgi/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div class="header">
+   <h1>Hello World!</h1>
+   <h2>SignUp</h2>
+   <h2>Login</h2>
+   </div>
+  )
+}
+
+export default Header
